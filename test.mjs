1d305f45a8eb9494a12f2a631fb231ac2a2e91ec
Kai Zhu <kaizhu256@gmail.com>
2024-03-18 11:33:35 -0500

    - jslint - Allow destructuring-assignment after function-definition.

--- test.mjs
+++ test.mjs
@@ -880,6 +880,17 @@ jstestDescribe((
                 return [
                     "let [\n    aa, bb = 0\n] = 0;\naa();\nbb();",
                     "let [...aa] = [...aa];\naa();",
+
+// PR-xxx - Allow destructuring-assignment after function-definition.
+
+                    (
+                        "let aa;\n"
+                        + "let bb;\n"
+                        + "function cc() {\n"
+                        + "    return;\n"
+                        + "}\n"
+                        + "[aa, bb] = cc();\n"
+                    ),
                     "let constructor = 0;\nconstructor();",
                     "let {\n    aa: bb\n} = 0;\nbb();",
                     "let {\n    aa: bb,\n    bb: cc\n} = 0;\nbb();\ncc();",
