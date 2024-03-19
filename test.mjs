227add76f48af1bbbfb122f3bf8c5bdb8084d615
Kai Zhu <kaizhu256@gmail.com>
2024-03-19 09:43:57 -0500

    const.fix

--- test.mjs
+++ test.mjs
@@ -879,7 +879,7 @@ jstestDescribe((
             ].map(function (directive) {
                 return [
                     "let [\n    aa, bb = 0\n] = 0;\naa();\nbb();",
-                    "let [...aa] = [...aa];\naa();",
+                    "let aa = 0;\nlet [...bb] = [...aa];\nbb();",
 
 // PR-xxx - Allow destructuring-assignment after function-definition.
 
