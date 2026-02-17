57ae2cba893aeb21851c8844b093b0ea0dcaa69a
Kai Zhu <kaizhu256@gmail.com>
2026-02-17 15:55:28 -0600

    tweak

--- jslint_ci.sh
+++ jslint_ci.sh
@@ -401,8 +401,8 @@ shCiBase() {(set -e
 # )}
     export GITHUB_BRANCH0="$(git branch --show-current)"
     # Auto-correct common errors in package.json.
-    # Check npm-version >= 9.0.0, before running npm-pkg-fix
-    if [ "$(npm --version | cut -d"." -f1)" -ge 9 ]
+    # Check npm-version >= 10.0.0, before running npm-pkg-fix
+    if [ "$(npm --version | cut -d"." -f1)" -ge 10 ]
     then
         npm pkg fix
     fi
